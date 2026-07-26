22cd93003903211cc78376f30ef88a0a74c7c173
Nirmal <24h010.nirmal@gmail.com>
2026-07-26 13:21:01 +0530

    Add files via upload

---
+++ src/data/products.js
@@ -0,0 +1,39 @@
+const products = [
+  {
+    id: 1,
+    name: "Wireless Headphones",
+    price: 2499,
+    category: "Electronics",
+    image:
+      "https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
+  },
+
+  {
+    id: 2,
+    name: "Smart Watch",
+    price: 3999,
+    category: "Electronics",
+    image:
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30"
+  },
+
+  {
+    id: 3,
+    name: "Men T-Shirt",
+    price: 799,
+    category: "Fashion",
+    image:
+      "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab"
+  },
+
+  {
+    id: 4,
+    name: "Sneakers",
+    price: 2999,
+    category: "Fashion",
+    image:
+      "https://images.unsplash.com/photo-1542291026-7eec264c27ff"
+  }
+];
+
+export default products;
